a235ee8b5c12e4c3f94b93ca4be911aa23b28ef8
Masataka Hisasue <masataka.hisasue@optim.co.jp>
2019-05-14 11:43:07 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "net/http"
+
+    "github.com/labstack/echo"
+)
+
+func main() {
+    e := echo.New()
+    e.POST("/", test)
+
+    e.Start(":8080")
+}
+
+type myStruct struct {
+    UserId string `json:"userid" form:"userid" query:"userid"`
+    PW string `json:"pw" form:"pw" query:"pw"`
+}
+
+func test(c echo.Context) error {
+    s := new(myStruct)
+    if err := c.Bind(s); err != nil {
+        fmt.Fprintln(os.Stderr, err)
+        return err
+    }
+
+    return c.NoContent(http.StatusOK)
+}
